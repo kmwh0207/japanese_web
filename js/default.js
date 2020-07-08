5e50be797fa052f7362dd1422e8651f81e982bb0
kmwh0207 <kmwh0207@naver.com>
2020-07-09 02:06:28 +0900

    main2 complete

--- js/default.js
+++ js/default.js
@@ -1,22 +1,51 @@
-window.onload = function(){
+let URLSearch;
+window.onload = function () {
     var img = document.getElementById("background");
-    document.body.style.width=img.width.toString()+"px"
-    document.body.style.height=img.height.toString()+"px"
+    document.body.style.width = img.width.toString() + "px"
+    document.body.style.height = img.height.toString() + "px"
+    URLSearch = new URLSearchParams(location.search);
 }
 /*document.body.style.width=window.innerWidth.toString()+"px"
 document.body.style.height=window.innerHeight.toString()+"px"*/
 
-class apply_img {
-    constructor(id, img_list) {
-        this._id = id;
-        this.img_list;
+function searchParam(key){
+    return new URLSearch.get(key);
+}
+
+function css(elem, prop, val) {
+    if (!(isElement(elem) && isString(prop) && isString(val))) {
+        throw new TypeError('css 파라미터 타입 에러');
+    }
+    elem.style[prop] = val;
+}
+
+class Add_img {
+    #_id;  
+    #_img_list; //private
+    #_anim;
+    #_event;
+    constructor(id, img_list, anim, event) {
+        if (isString(id) && isString(anim) && isArray(img_list) && isArray(event)) {
+            this._id = id;
+            this._img_list = img_list;
+            this._anim = anim;
+            this._event = event;
+        } else {
+            throw new Error("apply_img 파라미터 타입 에러");
+        }
     }
     apply() {
-        let elements = document.getElementById("container").childNodes;
-        for (const i of elements) {
-            i.
-            console.log(i);
+        let elements = document.getElementById(this._id).children;
+        for (let num=0; num<elements.length; num++) {
+            let element = elements[num];
+            let newElem = document.createElement('img');
+            newElem.addEventListener('click',this._event[num],false);
+            newElem.src = this._img_list[num];
+            newElem.classList.add(this._anim);
+            console.log(num);
+            element.appendChild(newElem);
         }
         console.log("apply img");
     }
 }
+
---
+++ js/main2_dev.js
@@ -0,0 +1,41 @@
+let imgList = ["img/mainmenu/1.color_eicon.png",
+"img/mainmenu/2.fruit_eicon.png",
+"img/mainmenu/3.family_eicon.png",
+"img/mainmenu/4.clothing_eicon.png",
+"img/mainmenu/5.drinks_eicon.png",
+"img/mainmenu/6.transpotation_eicon.png",
+"img/mainmenu/7.food_eicon.png",
+"img/mainmenu/8.body_parts_eicon.png"]
+
+var movepage = function(page){
+    document.getElementById("choice").style.display="block";
+    new Promise(function(resolve,reject){
+        document.getElementById("choice").classList.add("fadeIn");
+        document.getElementById("choice").addEventListener("touchend",function(e){
+            e.preventDefault();
+            console.log("touch");
+            if(e.changedTouches[0].pageX < document.body.style.width/2){
+                resolve(1);
+            }else{
+                resolve(2);
+            }
+        })
+    }).then(function(result){
+        document.getElementById("frame").setAttribute("src",page+"/?id="+result);
+        document.getElementById("frame").style.zIndex=500;
+    });
+}
+
+let eventList = [function(){movepage("game1.html")},
+function(){movepage("game2.html")},
+function(){movepage("game3.html")},
+function(){movepage("game4.html")},
+function(){movepage("game5.html")},
+function(){movepage("game6.html")},
+function(){movepage("game7.html")},
+function(){movepage("game8.html")},]
+
+window.addEventListener('load',function(){
+    let img_run = new Add_img("container",imgList,"fadeIn",eventList);
+    img_run.apply();
+});
