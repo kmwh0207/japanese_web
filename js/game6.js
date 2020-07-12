95a4625bde9123eefc074ef23c6a234e4b1673d3
kmwh0207 <kmwh0207@naver.com>
2020-07-13 01:20:51 +0900

    Todo game6.js

---
+++ js/game6.js
@@ -0,0 +1,145 @@
+var mode = 0;
+let selected_id = 0;
+
+let imgList = ['','','',"img/game/transport/transpotation_learn_1(비행기).png",'',"img/game/transport/transpotation_learn_1(트럭).png",'','',"img/game/transport/transpotation_learn_1(지하철).png",'','',"img/game/transport/transpotation_learn_1(오토바이).png","img/game/transport/transpotation_learn_1(택시).png",'',''];
+let imgList_ = ['','','','','',"img/game/transport/transpotation_learn_1(트럭).png",'','',"img/game/transport/transpotation_learn_1(지하철).png",'','',"img/game/transport/transpotation_learn_1(오토바이).png","img/game/transport/transpotation_learn_1(택시).png",'',''];
+
+let eventList = [
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "ひこうき   airplane";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "トラック   truck";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "ちかてつ 　subway ";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "オートバイ motocycle";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "タクシー   taxi";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    }
+];
+let eventList_ = [
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    },
+    function () {
+        document.getElementById("content").innerHTML = "";
+    }
+];
+
+window.addEventListener('load', function () {
+    nextpage(0);
+});
+
+function addTouchEvent(id){
+    let elements = document.getElementById(id).children;
+    for(let element of elements){
+        element.addEventListener("touchstart",handleStart,false);
+        element.addEventListener("touchmove",handleMove,false);
+        element.addEventListener("touchend",handleEnd,false);
+    }
+}
+function handleStart(event){
+    selected_id = this.dataset.num;
+}
+function handleMove(event){
+    
+}
+function handleEnd(event){
+    this.classList.remove("rotateRight");
+    selected_id = -1;
+}
+
+function nextpage(change) {
+    if (change > 0) {
+        mode = mode == 0 ? 1 : 0;
+        /* let elements = document.getElementsByClassName("color");
+        for(let element of elements){
+            element.classList.add("hidden");
+        } */
+    }
+    addTouchEvent("container5");
+    let elem = document.getElementsByClassName("item");
+    for (let i of elem) {
+        i.innerHTML = "";
+    }
+    if (mode == 0) {
+        document.getElementById("background").setAttribute("src","img/game/transport/transpotation_learn_1(background).png");
+        new Add_img("container5", imgList, "opacity", eventList).apply();
+    } else {
+        document.getElementById("background").setAttribute("src","img/game/transport/transpotation_learn_2(background).png");
+        new Add_img("container5", imgList_, "opacity", eventList_).apply();
+    }
+}
---
+++ js/game7.js
@@ -0,0 +1,90 @@
+img/game/transport/transpotation_learn_1(background).png
+var mode = 0;
+let selected_id = 0;
+
+let imgList1 = ["img/game/fruit/strawberry.png", "img/game/fruit/watermelon.png"];
+
+let eventList1 = [
+    function () {
+        this.src = this.src.slice(0, -5) + "_.png";
+        document.getElementById("content").innerHTML = "イチゴ";
+    },
+    function () {
+        this.src = this.src.slice(0, -5) + "_.png";
+        document.getElementById("content").innerHTML = "すいか";
+    }
+];
+
+let imgList2 = ["img/game/fruit/banana.png", "img/game/fruit/graphe.png", "img/game/fruit/persimmon.png"];
+
+let eventList2 = [
+    function () {
+        this.src = this.src.slice(0, -5) + "_.png";
+        document.getElementById("content").innerHTML = "バナナ";
+    },
+    function () {
+        this.src = this.src.slice(0, -5) + "_.png";
+        document.getElementById("content").innerHTML = "ぶどう";
+    },
+    function () {
+        this.src = this.src.slice(0, -5) + "_.png";
+        document.getElementById("content").innerHTML = "かぎ";
+    }
+];
+
+
+let imgList1_ = ["img/game/fruit/apple.png", "img/game/fruit/orange.png"];
+
+let eventList1_ = [
+    function () {
+        this.src = this.src.slice(0, -5) + "_.png";
+        document.getElementById("content").innerHTML = "りんご";
+    },
+    function () {
+        this.src = this.src.slice(0, -5) + "_.png";
+        document.getElementById("content").innerHTML = "オリンジ";
+    }
+];
+
+let imgList2_ = ["img/game/fruit/peach.png", "img/game/fruit/pear.png", "img/game/fruit/pineapple.png"];
+
+let eventList2_ = [
+    function () {
+        this.src = this.src.slice(0, -5) + "_.png";
+        document.getElementById("content").innerHTML = "もも";
+    },
+    function () {
+        this.src = this.src.slice(0, -5) + "_.png";
+        document.getElementById("content").innerHTML = "なし";
+    },
+    function () {
+        this.src = this.src.slice(0, -5) + "_.png";
+        document.getElementById("content").innerHTML = "パイナップル";
+    }
+];
+
+
+window.addEventListener('load', function () {
+    nextpage(0);
+});
+
+function nextpage(change) {
+    if (change > 0) {
+        mode = mode == 0 ? 1 : 0;
+        /* let elements = document.getElementsByClassName("color");
+        for(let element of elements){
+            element.classList.add("hidden");
+        } */
+    }
+    let elem = document.getElementsByClassName("item");
+    for (let i of elem) {
+        i.innerHTML = "";
+    }
+    if (mode == 0) {
+        new Add_img("container2", imgList1, "opacity", eventList1).apply();
+        new Add_img("container3", imgList2, "opacity", eventList2).apply();
+    } else {
+        new Add_img("container2", imgList1_, "opacity", eventList1).apply();
+        new Add_img("container3", imgList2_, "opacity", eventList2).apply();
+    }
+}
