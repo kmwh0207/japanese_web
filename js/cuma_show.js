43bd609b6aa73df59482a18afba74dc99420de13
kmwh0207 <kmwh0207@naver.com>
2020-08-01 17:53:20 +0900

    update cuma_show.js

--- js/cuma_show.js
+++ js/cuma_show.js
@@ -5,7 +5,7 @@ window.addEventListener('load', function () {
         //let link = new RegExp(this.src.split("/").pop());
         if (link.match('_2.') || link.match('subgame')) {
             let cuma = document.createElement("img");
-            cuma.setAttribute('style', "position:absolute; z-index:2000; bottom:0; right:10%; width:20%;");
+            cuma.setAttribute('style', "position:absolute; z-index:2000; bottom:0; right:15%; width:20%;");
             cuma.classList.add("cuma_show");
             document.body.appendChild(cuma);
         }
